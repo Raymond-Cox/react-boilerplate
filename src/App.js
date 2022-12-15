f2c35bf8921bb1f51dcf3308e5701c7455c84182
Raymond Cox <raymondc1911@gmail.com>
2022-12-14 21:11:28 -0500

    Stripped out SVG stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import './App.css'
-import Logo from './assets/logo.svg'
 
 export function App() {
-    return (
-        <div className="container">
-            <Logo className="logo" />
-            <h1>Hello world!</h1>
-        </div>
-    )
+    return <h1>Hello world!</h1>
 }
